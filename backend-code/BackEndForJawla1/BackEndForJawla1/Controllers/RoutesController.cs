2e7efabeab3dea85962bf0f46f3bf61b6eab8154
daniah abukabeer <daniahmabukabeer@gmail.com>
2024-05-29 23:52:51 +0300

    Removed embeded repository and add its content back as normal repo

---
+++ backend-code/BackEndForJawla1/BackEndForJawla1/Controllers/RoutesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using BackEndForJawla1.Models;
+using BackEndForJawla1.Data;
+using BackEndForJawla1.services;
+
+namespace BackEndForJawla1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutesController : ControllerBase
+    {
+        
+        private readonly RoutesService _routesService;
+
+        public RoutesController(RoutesService routesService) {
+            _routesService = routesService;
+        }
+
+
+        //Post : api/routes
+        [HttpPost]
+        public IActionResult AddRoute(routes routes)
+        {
+            _routesService.CreateRouteAsync(routes);
+            return Ok();
+        }
+
+        //get: api/routes
+        [HttpGet]
+        public IActionResult GetRoute(string id) { }
+       
+
+    }
+}
---
+++ backend-code/BackEndForJawla1/BackEndForJawla1/services/IRouteService.cs
@@ -0,0 +1,13 @@
+﻿using BackEndForJawla1.Models;
+
+namespace BackEndForJawla1.services
+{
+    public interface IRouteService
+    {
+        Task<routes> CreateRouteAsync(routes route);
+        Task<routes> GetRouteByIdAsync(string id);
+        Task<IEnumerable<routes>> GetAllRoutesAsync();
+        Task<routes> UpdateRouteAsync(routes routes);
+        Task<bool> DeleteRouteAsync(string id);
+    }
+}
