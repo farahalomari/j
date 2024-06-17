2f972050af4ff86b09efc26a37584cac4e482e89
LeenAbuSharaf <167311947+LeenAbuSharaf@users.noreply.github.com>
2024-06-17 20:34:15 +0300

    Add files via upload

---
+++ backend-code/BackEndForJawla1-master/BackEndForJawla1-master/BackEndForJawla1/Migrations/20240606063147_postgresql.container_migration_970.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEndForJawla1.Migrations
+{
+    /// <inheritdoc />
+    public partial class postgresqlcontainer_migration_970 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_stopsgeojson",
+                schema: "busNetwork",
+                table: "stopsgeojson");
+
+            migrationBuilder.EnsureSchema(
+                name: "user");
+
+            migrationBuilder.RenameTable(
+                name: "stopsgeojson",
+                schema: "busNetwork",
+                newName: "user",
+                newSchema: "user");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_user",
+                schema: "user",
+                table: "user",
+                column: "routeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_user",
+                schema: "user",
+                table: "user");
+
+            migrationBuilder.RenameTable(
+                name: "user",
+                schema: "user",
+                newName: "stopsgeojson",
+                newSchema: "busNetwork");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_stopsgeojson",
+                schema: "busNetwork",
+                table: "stopsgeojson",
+                column: "routeID");
+        }
+    }
+}
---
+++ backend-code/BackEndForJawla1-master/BackEndForJawla1-master/BackEndForJawla1/Models/user.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BackEndForJawla1.Models
+{
+    public class user
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int userID { get; set; }
+
+      
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"00962(78|77|79)\d{7}", ErrorMessage = "Invalid phone number")]
+        public string phoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Password must be at least 8 characters long and include at least one letter, one number, and one special character.")]
+        public string password { get; set; }
+
+    }
+}
